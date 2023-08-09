47bdd7b8638006cdb8573914a40883a33591492c
Daniela Moraes <97682748+tokyohmachine@users.noreply.github.com>
2023-08-09 19:27:56 -0300

    Add files via upload

---
+++ Students Form/public/src/App.js
@@ -0,0 +1,13 @@
+import './components/App.css';
+import StudentForm from './components/StudentForm.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
